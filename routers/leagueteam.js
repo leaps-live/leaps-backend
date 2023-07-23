fa33abbd6fdd8114dd3f12f6eaddff3f9016d82b
Dongchunhua1225 <hdong25@uw.edu>
2023-07-24 00:00:57 +0800

    league team

---
+++ routers/leagueteam.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+
+//7/24
+//add team to the league
+
+//delete the team from league
+
+//edit team category
+
+//get all the team from the league
+
+module.exports = router;
