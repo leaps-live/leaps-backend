8c0ce35e4202a944aca0d85d2cb22524dc37b5d7
Dongchunhua1225 <hdong25@uw.edu>
2023-08-01 13:15:52 +0800

    8/1 user route

--- routers/users.js
+++ routers/users.js
@@ -6,7 +6,8 @@ const pool = require("../db");
 router.post("/register", async (req, res) => {
   try {
     const {
-      userFormalName,
+      userFirstName,
+      userLastName,
       username,
       userEmail,
       userPassword,
@@ -26,9 +27,10 @@ router.post("/register", async (req, res) => {
     }
 
     let newUser = await pool.query(
-      "INSERT INTO tbl_user (userFormalName, username, userEmail, userPassword, userBirthday, userHeight, userWeight) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      "INSERT INTO tbl_user (userFirstName, userLastName, username, userEmail, userPassword, userBirthday, userHeight, userWeight) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
       [
-        userFormalName,
+        userFirstName,
+        userLastName,
         username,
         userEmail,
         userPassword,
@@ -57,6 +59,33 @@ router.post("/login", async (req, res) => {
 
     if (user.rows.length === 0) {
       console.log("wrong email");
+      return res.status(401).json("Invalid Credentials");
+    }
+
+    if (user.rows[0].userPassword !== userPassword) {
+      console.log("wrong password");
+      return res.status(401).json("Invalid Credential");
+    }
+
+    return res.json(user.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+//loging by username
+router.post("/login/username", async (req, res) => {
+  try {
+    const { username, userPassword } = req.body;
+
+    const user = await pool.query(
+      "SELECT * FROM tbl_user WHERE username = $1",
+      [username]
+    );
+
+    if (user.rows.length === 0) {
+      console.log("wrong username");
       return res.status(401).json("Invalid Credentials");
     }
 
@@ -126,7 +155,6 @@ router.get("/:userid", async (req, res) => {
   }
 });
 
-
 //search by name[get route]
 router.get("/username/:userName", async (req, res) => {
   try {
@@ -138,6 +166,25 @@ router.get("/username/:userName", async (req, res) => {
     );
 
     res.json(getName.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//edit user profile
+router.put("/:userid", async (req, res) => {
+  try {
+    const { userid } = req.params;
+
+    const { userBirthday, userHeight, userWeight } = req.body;
+
+    const editRegularProfile = pool.query(
+      "SET userBirthday = $1, userHeight = $2, userWeight = $3 FROM tbl_user WHERE userid = $4 RETURNING *",
+      [userBirthday, userHeight, userWeight, userid]
+    );
+
+    res.json(editRegularProfile.rows[0]);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
