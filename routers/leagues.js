ddbc2228fc5eeb0713c978a2f4b21491999a4a1d
tangjingyu0812 <jingyuta@usc.edu>
2023-08-14 13:45:00 +0800

    feat: add routes from v1 to this branch

--- routers/leagues.js
+++ routers/leagues.js
@@ -190,4 +190,41 @@ router.get("/get/:leagueid", async (req, res) => {
   }
 })
 
+//get user's league from the team
+router.get("/getLeague/:userid", async (req, res) => {
+  try {
+    const { userid } = req.params;
+
+    const getAllTeam = await pool.query(
+      "SELECT * FROM tbl_team_players WHERE userid = $1",
+      [userid]
+    );
+
+    //use the return value from the get all team 
+    const teamIds = getAllTeam.rows.map(item => item.teamid);
+    //const leagueIds = [];
+    const leagueInfo = [];
+
+    for(const teamid of teamIds) {
+      const leagueinfo = await pool.query(
+        "SELECT * FROM tbl_league l \
+      JOIN tbl_team_league tl ON l.leagueid = tl.leagueid  \
+      WHERE tl.teamid = $1",
+        [teamid]
+      );
+      leagueInfo.push(leagueinfo.rows);
+    }
+
+    //get all league info
+    // for(const leagueid of leagueIds) {
+    //   let leagueinfo = await pool.query("SELECT * FROM tbl_league WHERE leagueid = $1", l)
+    // }
+
+    res.json(leagueInfo);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
