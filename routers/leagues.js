87b6f4810b1fa2a70a94801a3f3e1ae5be0d6ef6
Julius <juliuscecilia33@gmail.com>
2023-09-11 23:50:56 -0700

    get leagues a user is part of

--- routers/leagues.js
+++ routers/leagues.js
@@ -197,18 +197,21 @@ router.post("/search/leaguename", async (req, res) => {
 //get all league info
 router.get("/get/:leagueid", async (req, res) => {
   try {
-    const {leagueid} = req.params;
+    const { leagueid } = req.params;
 
-    const getAllInfo = await pool.query("SELECT * FROM tbl_league WHERE leagueid = $1", [leagueid]);
+    const getAllInfo = await pool.query(
+      "SELECT * FROM tbl_league WHERE leagueid = $1",
+      [leagueid]
+    );
 
     res.json(getAllInfo.rows[0]);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
-})
+});
 
-//get user's league from the team
+//get the leagues a user is a part of
 router.get("/getLeague/:userid", async (req, res) => {
   try {
     const { userid } = req.params;
@@ -218,27 +221,19 @@ router.get("/getLeague/:userid", async (req, res) => {
       [userid]
     );
 
-    //use the return value from the get all team 
-    const teamIds = getAllTeam.rows.map(item => item.teamid);
-    //const leagueIds = [];
-    const leagueInfo = [];
+    const teamIds = getAllTeam.rows.map((item) => item.teamid);
 
-    for(const teamid of teamIds) {
-      const leagueinfo = await pool.query(
-        "SELECT * FROM tbl_league l \
-      JOIN tbl_team_league tl ON l.leagueid = tl.leagueid  \
-      WHERE tl.teamid = $1",
+    const userLeagues = [];
+
+    for (const teamid of teamIds) {
+      const teaminfo = await pool.query(
+        "SELECT * FROM tbl_team_league WHERE teamid = $1",
         [teamid]
       );
-      leagueInfo.push(leagueinfo.rows);
+      userLeagues.push(teaminfo.rows);
     }
 
-    //get all league info
-    // for(const leagueid of leagueIds) {
-    //   let leagueinfo = await pool.query("SELECT * FROM tbl_league WHERE leagueid = $1", l)
-    // }
-
-    res.json(leagueInfo);
+    res.json(userLeagues);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
